d6ecff854f26ab486b29f0b26a8dd2866291a815
Vaibhav Dubey <vaibhavdubey1419@gmail.com>
2023-05-20 19:17:49 +0530

    Hoisting Finish

--- Hosting/app.js
+++ Hosting/app.js
@@ -1,8 +1,12 @@
 getName();
-console.log(x)
-console.log(getName())
+console.log(getName1)
+console.log(getName)
 
 var x = 7;
 function getName(){
     console.log("Namaste Js")
+}
+
+var getName1 = function(){
+    console.log("NJ-2")
 }
